f7ff1727114c378f76348b6a36cef85ddda3b1f1
LittleEndianRoot <littleendianroot@gmail.com>
2019-04-04 22:26:58 +0100

    Showing the screen color changing animation

--- SDLAnimatingColors/src/SDLAnimatingColors.cpp
+++ SDLAnimatingColors/src/SDLAnimatingColors.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <SDL.h>
+#include <math.h>
 #include "Screen.h"
 
 using namespace std;
@@ -28,11 +29,16 @@ int main()
 		// Update particles
 
 		// draw particles
-		for(int y = 200; y < Screen::SCREEN_HEIGHT-200; y++)
+		int elapsedTime = SDL_GetTicks();
+		unsigned char red = (unsigned char)((1 + cos(elapsedTime * 0.0003)) * 128);
+		unsigned char green = (unsigned char)((1 + sin(elapsedTime * 0.0001)) * 128);
+		unsigned char blue = (unsigned char)((1 + sin(elapsedTime * 0.005)) * 128);
+
+		for(int y = 0; y < Screen::SCREEN_HEIGHT; y++)
 		{
-			for(int x = 300; x < Screen::SCREEN_WIDTH-300; x++)
+			for(int x = 0; x < Screen::SCREEN_WIDTH; x++)
 			{
-				screen.setPixel(x, y, 128, 0, 255);
+				screen.setPixel(x, y, red, green, blue);
 			}
 		}
 		// draw screen
