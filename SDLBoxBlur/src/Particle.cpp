f076f5ef063de3067784f38ac1d71d39f4acfff4
LittleEndianRoot <littleendianroot@gmail.com>
2019-04-13 15:57:33 +0100

    Added Box blur to our particle explosion

---
+++ SDLBoxBlur/src/Particle.cpp
@@ -0,0 +1,36 @@
+/*
+ * Particle.cpp
+ *
+ *  Created on: 12 Apr 2019
+ *      Author: endian
+ */
+#include <stdlib.h>
+#include <math.h>
+#include "Particle.h"
+
+namespace littleendianroot
+{
+
+Particle::Particle():
+		m_x(0), m_y(0)
+{
+	m_speed = (0.0001 * rand())/RAND_MAX;
+	m_direction = (2.0 * M_PI * rand())/RAND_MAX;
+
+}
+
+Particle::~Particle()
+{
+
+}
+
+void Particle::update(int interval)
+{
+	double xspeed = m_speed * cos(m_direction);
+	double yspeed = m_speed * sin(m_direction);
+
+	m_x += xspeed * interval;
+	m_y += yspeed * interval;
+}
+
+} /* namespace littleendianroot */
---
+++ SDLBoxBlur/src/SDLBoxBlur.cpp
@@ -0,0 +1,64 @@
+//============================================================================
+// Name        : SDLBoxBlur.cpp
+// Author      : endian
+// Version     :
+// Copyright   : Your copyright notice
+// Description :
+//============================================================================
+
+#include <iostream>
+#include <math.h>
+#include "Screen.h"
+#include "Swarm.h"
+
+using namespace std;
+using namespace littleendianroot;
+
+int main()
+{
+	srand(time(NULL));
+
+	Screen screen;
+	Swarm swarm;
+
+	if(screen.init() == false)
+	{
+		cout << "Failed to initialize SDL" << endl;
+	}
+
+	while(true)
+	{
+		int elapsedTime = SDL_GetTicks();
+
+
+		swarm.update(elapsedTime);
+
+		unsigned char red = (unsigned char)((1 + cos(elapsedTime * 0.0003)) * 128);
+		unsigned char green = (unsigned char)((1 + sin(elapsedTime * 0.001)) * 128);
+		unsigned char blue = (unsigned char)((1 + sin(elapsedTime * 0.0005)) * 128);
+
+		const Particle *const pParticle = swarm.getParticle();
+
+		for(int i = 0; i < Swarm::NPARTICLES; i++)
+		{
+			Particle particle = pParticle[i];
+			int x = (particle.m_x + 1) * Screen::SCREEN_WIDTH/2;
+			int y = particle.m_y * Screen::SCREEN_WIDTH/2 + Screen::SCREEN_HEIGHT/2;
+
+			screen.setPixel(x, y, red, green, blue);
+		}
+
+		screen.boxBlur();
+
+		screen.update();
+
+		if(screen.processEvents() == false)
+		{
+			break;
+		}
+	}
+
+	screen.close();
+
+	return 0;
+}
